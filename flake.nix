879b5c819661f22f636e2f1a0f691773ade36c58
drnfc <djbatfrog@gmail.com>
2024-03-13 22:46:41 -0700

    Actually got the build working

--- flake.nix
+++ flake.nix
@@ -1,24 +1,53 @@
 {
   inputs = {
-     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-     systems.url = "github:nix-systems/default";
+     dream2nix.url = "github:nix-community/dream2nix";
      flake-utils.url = "github:numtide/flake-utils";
+     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+     nixpkgs.follows = "dream2nix/nixpkgs";
+     systems.url = "github:nix-systems/default";
   };
 
   outputs = {
     self,
+    dream2nix,
+    flake-compat,
     flake-utils,
     nixpkgs,
     systems,
     ...
   } @ inputs: 
-  flake-utils.lib.eachDefaultSystem (system:
+  inputs.flake-utils.lib.eachDefaultSystem (system:
   let
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
   in rec {
+    packages.default = dream2nix.lib.evalModules {
+      packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
+      modules = [
+        ./default.nix
+        {
+          paths.projectRoot = ./.;
+          paths.projectRootFile = "flake.nix";
+          paths.package = ./.;
+        }
+      ];
+    };
+
+    packages.entrypoint = pkgs.writeShellScriptBin "entrypoint" ''
+      ${pkgs.nodejs}/bin/node ${packages.default}/lib/node_modules/pda/main.js
+    '';
+
+    packages.oci = pkgs.dockerTools.buildImage {
+      name = "PDA";
+      config = {
+        cmd = [ "${packages.entrypoint}/bin/entrypoint" ];
+      };
+    };
+
     devShells = {
       default = pkgs.mkShell {
         buildInputs = [
+          pkgs.node2nix
           pkgs.nodejs
           # You can set the major version of Node.js to a specific one instead
           # of the default version
@@ -34,20 +63,6 @@
         shellHook = ''
             zsh
             exit
-        '';
-      };
-    };
-    packages = rec {
-      default = pkgs.stdenv.mkDerivation {
-        name = "PDA";
-        src = ./pda;
-        buildInputs = with pkgs; [ nodejs ];
-        buildPhase = ''
-          mkdir $out/
-          npm install
-        '';
-        installPhase = ''
-          cp -r ./. $out/
         '';
       };
     };
