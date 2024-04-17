534469265f2ac31fdbadb9875e12c7e127dad1b3
drnfc <djbatfrog@gmail.com>
2024-04-16 23:50:53 -0700

    fix(docker): made constants work for the docker container

--- src/server/constants.js
+++ src/server/constants.js
@@ -1,9 +1,11 @@
+import { env } from "process";
+
 //webserver
 export const PORT = 6969;
 
 //database
-export const DB_PORT = 7734;
-export const DB_NAME = process.env.DB_NAME || "pdaDB";
-export const DB_USER = process.env.DB_USER || "pdaAdmin";
+export const DB_PORT = process.env.DB_PORT || 7734;
+export const DB_NAME = process.env.POSTGRES_DB || "pdaDB";
+export const DB_USER = process.env.POSTGRES_USER || "pdaAdmin";
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PASSWORD = process.env.DB_HOST || "postgres";
+export const DB_PASSWORD = process.env.POSTGRES_PASSWORD || "postgres";
