fc26a8928330cd5683ed3cfcc9519f5571d90cdd
drnfc <djbatfrog@gmail.com>
2024-04-16 23:52:13 -0700

    refactor(constants): reordered
    just made the order the same as the docker compose file

--- src/server/constants.js
+++ src/server/constants.js
@@ -4,8 +4,8 @@ import { env } from "process";
 export const PORT = 6969;
 
 //database
-export const DB_PORT = process.env.DB_PORT || 7734;
-export const DB_NAME = process.env.POSTGRES_DB || "pdaDB";
 export const DB_USER = process.env.POSTGRES_USER || "pdaAdmin";
-export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PASSWORD = process.env.POSTGRES_PASSWORD || "postgres";
+export const DB_NAME = process.env.POSTGRES_DB || "pdaDB";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_PORT = process.env.DB_PORT || 7734;
