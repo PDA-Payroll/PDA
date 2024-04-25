9388c0d786531d763b40c841aae44fa571a69aba
drnfc <djbatfrog@gmail.com>
2024-04-25 11:46:45 -0700

    feat(EmployeeSchema): Added supervisor id

--- src/db/schema/EmployeeSchema.js
+++ src/db/schema/EmployeeSchema.js
@@ -2,32 +2,36 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../dbIndex.js";
 
 export const Employee = sequelize.define("Employee", {
-  employeeFirstName: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  employeeMiddleName: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  employeeLastName: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  employeeUserName: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  employeePassword: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  isAdmin: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  socialSecurityNumber: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+	employeeFirstName: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+	employeeMiddleName: {
+		type: DataTypes.TEXT,
+		allowNull: true,
+	},
+	employeeLastName: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+	employeeUserName: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+	employeePassword: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+	isAdmin: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false,
+	},
+	socialSecurityNumber: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	supervisorId: {
+		type: DataTypes.INTEGER,
+		allowNull: true,
+	},
 });
