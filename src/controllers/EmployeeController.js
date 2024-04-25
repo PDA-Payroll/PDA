e50778791ab628d3521e44ef0e29f83816ffa523
drnfc <djbatfrog@gmail.com>
2024-04-25 10:30:19 -0700

    style(biome): biome converted spaces to tabs

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -1,49 +1,49 @@
 import * as db from "../db/dbIndex.js";
 import {
-  deleteAll,
-  deleteObjectById,
-  findObjectByPk,
+	deleteAll,
+	deleteObjectById,
+	findObjectByPk,
 } from "../lib/controllerLib.js";
 
 const Employee = db.Employee;
 const Op = db.Op;
 
 export const create = (req, res) => {
-  const employee = {
-    employeeFirstName: req.body.employeeFirstName,
-    employeeMiddleName: req.body.employeeMiddleName,
-    employeeLastName: req.body.employeeMiddleName,
-    employeeUserName: req.body.employeeUserName,
-    employeePassword: req.body.employeePassword,
-    isAdmin: req.body.isAdmin,
-    socialSecurityNumber: req.body.socialSecurityNumber,
-  };
-  Employee.create(employee)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Error occurred while creating Employee",
-      });
-    });
+	const employee = {
+		employeeFirstName: req.body.employeeFirstName,
+		employeeMiddleName: req.body.employeeMiddleName,
+		employeeLastName: req.body.employeeMiddleName,
+		employeeUserName: req.body.employeeUserName,
+		employeePassword: req.body.employeePassword,
+		isAdmin: req.body.isAdmin,
+		socialSecurityNumber: req.body.socialSecurityNumber,
+	};
+	Employee.create(employee)
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || "Error occurred while creating Employee",
+			});
+		});
 };
 
 export const findEmployeeByPk = findObjectByPk(Employee);
 
 export const findEmployeeByUsername = (req, res) => {
-  const username = req.query.employeeUserName;
-  var condition = username ? { username: { [Op.like]: `%${title}%` } } : null;
+	const username = req.query.employeeUserName;
+	var condition = username ? { username: { [Op.like]: `%${title}%` } } : null;
 
-  Employee.findOne({ where: condition })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "An error occurred while searching username",
-      });
-    });
+	Employee.findOne({ where: condition })
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || "An error occurred while searching username",
+			});
+		});
 };
 
 export const deleteEmployeeById = deleteObjectById(Employee);
