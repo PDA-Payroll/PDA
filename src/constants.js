fb402619febf5e4e0ce87102d06feae3361f835f
drnfc <djbatfrog@gmail.com>
2024-04-30 15:49:06 -0700

    chore(database): switched to my server computer

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@ export const PORT = 6969;
 export const DB_USER = process.env.POSTGRES_USER || "pdaAdmin";
 export const DB_PASSWORD = process.env.POSTGRES_PASSWORD || "postgres";
 export const DB_NAME = process.env.POSTGRES_DB || "pdaDB";
-export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_HOST = process.env.DB_HOST || "zbecker.cc";
 export const DB_PORT = process.env.DB_PORT || 7734;
