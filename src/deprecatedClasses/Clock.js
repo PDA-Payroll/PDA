0032d8e88aeda7aa464c2ff515df57bbeeb40bc2
drnfc <djbatfrog@gmail.com>
2024-05-01 14:51:37 -0700

    chore: removed deprecatedClasses

--- src/deprecatedClasses/Clock.js
+++
@@ -1,8 +0,0 @@
-class Clock{
-
-    get time(){//returns the current time
-        const now = new Date();
-        const time = now.getTime();
-        return time;
-    }
-}
--- src/deprecatedClasses/Management.js
+++
@@ -1,9 +0,0 @@
-class Management extends Employee{
-    constructor(hourlyRate, employeeID,managementLevel) {
-        super(hourlyRate, employeeID);
-        this.managementLevel = managementLevel;
-    }
-    getManagementLevel(){
-        return this.managementLevel;
-    }
-}
